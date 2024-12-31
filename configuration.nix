a3d83ce5269e97de40d5436e60d63ec860b27802
Cole Faust <colecfaust@gmail.com>
2024-12-30 17:35:12 -0800

    Finish setup
    Can now build either system and Runelite will be on the
    desktop already, ready to launch.

--- configuration.nix
+++ configuration.nix
@@ -1,7 +1,9 @@
 {
   config,
+  lib,
   pkgs,
   is2411,
+  home-manager,
   ...
 }:
 let
@@ -11,7 +13,26 @@ let
   specificTo2405 = if !is2411 then {
     hardware.opengl.enable = true;
   } else {};
+
+  runelite = pkgs.stdenv.mkDerivation {
+      name = "runelite";
+
+      src = pkgs.fetchurl {
+          name = "runelite";
+          url = "https://github.com/runelite/launcher/releases/download/2.7.2/RuneLite.AppImage";
+          sha256 = "sha256-lAGCngrbEw1Yhsw/m5W1xj+gtNtJQxgentUlYUOFgec=";
+      };
+
+      phases = [ "installPhase" ];
+
+      installPhase = ''
+          install -D $src $out
+      '';
+  };
+in
 {
+  imports = [home-manager.nixosModules.home-manager];
+
   nix.settings.experimental-features = [
     "nix-command"
     "flakes"
@@ -28,5 +49,36 @@ let
   };
   services.libinput.enable = true;
   services.xserver.desktopManager.xfce.enable = true;
+
+  users.mutableUsers = false;
+  users.users.root.initialPassword = "password";
+  users.users.myuser = {
+    isNormalUser = true;
+    initialPassword = "password";
+    extraGroups = [
+      "wheel"
+      "networkmanager"
+      "audio"
+    ];
+  };
+  services.displayManager.autoLogin.user = "myuser";
+
+  home-manager.useGlobalPkgs = true;
+  home-manager.useUserPackages = true;
+  home-manager.users.myuser = {pkgs, ... }: {
+    home.file."Desktop/Runelite".source = runelite;
+    home.stateVersion = "24.05";
+  };
+
+  boot.binfmt.registrations.appimage = {
+    wrapInterpreterInShell = false;
+    interpreter = "${pkgs.appimage-run}/bin/appimage-run";
+    recognitionType = "magic";
+    offset = 0;
+    mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
+    magicOrExtension = ''\x7fELF....AI\x02'';
+  };
+
+  system.stateVersion = "24.05";
 
 } // specificTo2411 // specificTo2405
--- flake.nix
+++ flake.nix
@@ -2,6 +2,14 @@
   inputs = {
     nixpkgs2411.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs2405.url = "github:nixos/nixpkgs/nixos-24.05";
+    home-manager-2411 = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs2411";
+    };
+    home-manager-2405 = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs2405";
+    };
   };
 
   outputs =
@@ -9,6 +17,8 @@
       self,
       nixpkgs2411,
       nixpkgs2405,
+      home-manager-2411,
+      home-manager-2405,
       ...
     }:
     {
@@ -17,12 +27,14 @@
           modules = [ ./configuration.nix ];
           specialArgs = {
             is2411 = true;
+            home-manager = home-manager-2411;
           };
         };
         mysystem2405 = nixpkgs2405.lib.nixosSystem {
           modules = [ ./configuration.nix ];
           specialArgs = {
             is2411 = false;
+            home-manager = home-manager-2405;
           };
         };
       };
